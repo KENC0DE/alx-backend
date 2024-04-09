8ad3455e5131d9ab9b10f04c77e47108cf80591c
KENC0DE <kenansameseret@gmail.com>
2024-04-09 19:11:50 +0530

    what is this

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -23,13 +23,18 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """locale getter"""
-    request.accept_languages.best_match(app.config['LANGUAGES'])
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
     """set lang"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
