f7effb199f3dae32424f68028f3a4e89ef3fe332
KENC0DE <kenansameseret@gmail.com>
2024-04-09 23:12:21 +0530

    done

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -2,7 +2,7 @@
 """
 Bable
 """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
@@ -20,12 +20,46 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """
+    returns a user dictionary or None
+    if the ID cannot be found or if login_as was not passed
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """Executes before request"""
+    user = get_user()
+    g.user = user
+
+
 @babel.localeselector
 def get_locale():
     """locale getter"""
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
-        print(locale)
+        return locale
+
+    if g.user:
+        locale = g.user.get('locale')
+        if locale and locale in app.config['LANGUAGES']:
+            return locale
+
+    locale = request.headers.get('locale', None)
+    if locale in app.config['LANGUAGES']:
         return locale
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
@@ -34,7 +68,7 @@ def get_locale():
 @app.route('/')
 def index():
     """set lang"""
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
