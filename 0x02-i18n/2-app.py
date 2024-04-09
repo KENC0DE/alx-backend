37d59f46f69c861bcd5f2c486975a223bd501869
KENC0DE <kenansameseret@gmail.com>
2024-04-09 18:12:55 +0530

    check

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,7 +2,7 @@
 """
 Bable
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -19,10 +19,16 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """locale getter"""
+    request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     """set lang"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
