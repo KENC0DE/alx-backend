4c2cb1fe600b48cf0d76b517f452d25e15545b28
KENC0DE <kenansameseret@gmail.com>
2024-04-10 00:05:38 +0530

    hmm

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,41 +1,23 @@
 #!/usr/bin/env python3
-"""A Flask application"""
-from flask import Flask, g, render_template, request
+"""
+Bable
+"""
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
-from typing import Dict, Union
-
-# Create the app
-app = Flask(__name__)
 
 
-class Config:
-    """Flask app configuration class"""
+class Config(object):
+    """Babel configuration"""
+
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# Configure the app
+app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
-
-# Set up Babel
 babel = Babel(app)
-
-
-@babel.localeselector
-def get_locale():
-    """Choose the best language to serve among the supported ones
-    or the one specified in 'locale' URL parameter if given and supported
-    """
-    # If we have 'locale' in the request arguments
-    # and this language is supported, return it
-    lang = request.args.get('locale')
-    if lang and lang in app.config['LANGUAGES']:
-        return lang
-
-    # Return the default preference
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 users = {
@@ -46,29 +28,38 @@ users = {
 }
 
 
-def get_user() -> Union[Dict, None]:
-    """Return a user dictionary, or None if the ID
-    cannot be found or if login_as was not passed
+def get_user():
     """
-    try:
-        user_id = int(request.args.get('login_as'))
-    except BaseException:
-        return None
-
-    return users.get(user_id)
+    returns a user dictionary or None
+    if the ID cannot be found or if login_as was not passed
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
 
 
 @app.before_request
 def before_request() -> None:
-    """Should use get_user to find a user if any,
-    and set it as a global on flask.g.user
-    """
-    g.user = get_user()
+    """Executes before request"""
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """locale getter"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
-    """The Index route"""
+    """set lang"""
     return render_template('5-index.html', user=g.user)
 
 
