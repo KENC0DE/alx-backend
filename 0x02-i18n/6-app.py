0b15d046d6040371380cce303b210afc335a62ea
KENC0DE <kenansameseret@gmail.com>
2024-04-09 22:57:18 +0530

    check

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -2,7 +2,7 @@
 """
 Bable
 """
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -20,35 +20,12 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-def get_user():
-    """
-    returns a user dictionary or None
-    if the ID cannot be found or if login_as was not passed
-    """
-    return None
-
-
-@app.before_request
-def before_request() -> None:
-    """Executes before request"""
-    user = get_user()
-    g.user = user
-
-
 @babel.localeselector
 def get_locale():
     """locale getter"""
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
-        return locale
-
-    if g.user:
-        locale = g.user.get('locale')
-        if locale and locale in app.config['LANGUAGES']:
-            return locale
-
-    locale = request.headers.get('locale', None)
-    if locale in app.config['LANGUAGES']:
+        print(locale)
         return locale
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
@@ -57,7 +34,7 @@ def get_locale():
 @app.route('/')
 def index():
     """set lang"""
-    return render_template('6-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
