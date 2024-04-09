71da7cfe79a98f362ba5c5a7f187d9439ba4e9b1
KENC0DE <kenansameseret@gmail.com>
2024-04-09 22:30:23 +0530

    whatever 5 m ll

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
+"""A simple flask app
 """
-Bable
-"""
+
+
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
 class Config(object):
-    """Babel configuration"""
+    """_summary_
 
+    Returns:
+                    _type_: _description_
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
@@ -29,9 +34,7 @@ users = {
 
 
 def get_user():
-    """
-    returns a user dictionary or None
-    if the ID cannot be found or if login_as was not passed
+    """returns a user dictionary or None if the ID cannot be found
     """
     login_id = request.args.get('login_as')
     if login_id:
@@ -41,14 +44,19 @@ def get_user():
 
 @app.before_request
 def before_request() -> None:
-    """Executes before request"""
+    """_summary_
+    """
     user = get_user()
     g.user = user
 
 
 @babel.localeselector
 def get_locale():
-    """locale getter"""
+    """_summary_
+
+    Returns:
+                    _type_: _description_
+    """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         print(locale)
@@ -56,12 +64,15 @@ def get_locale():
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+# babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route('/')
 def index():
-    """set lang"""
+    """_summary_
+    """
     return render_template('5-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port="5000", host="0.0.0.0", debug=True)
